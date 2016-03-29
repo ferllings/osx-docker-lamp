5ee61f05e5b88247cff6156804aa5374f5e1efd1
Ian Edington <IanEdington@gmail.com>
2016-03-29 13:57:59 -0400

    Enable setting which directory is served by apache
    APACHE_ROOT environment variable tells apache which directory in the
    /app folder to serve.
    
    ie. if $APACHE_ROOT is set to "web",
        `/app/web` will be symlinked to `/var/www/html`
    if $APACHE_ROOT is not set,
        `/app` will be symlinked to `/var/www/html`
    
    Also, fixes "Environment variables" list error in readme

--- run.sh
+++ run.sh
@@ -7,6 +7,10 @@ sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILE
 
 sed -i "s/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=staff/" /etc/apache2/envvars
 
+if [ -n "$APACHE_ROOT" ];then
+    rm -f /var/www/html && ln -s "/app/${APACHE_ROOT}" /var/www/html
+fi
+
 sed -i -e "s/cfg\['blowfish_secret'\] = ''/cfg['blowfish_secret'] = '`date | md5sum`'/" /var/www/phpmyadmin/config.inc.php
 
 if [ -n "$VAGRANT_OSX_MODE" ];then
